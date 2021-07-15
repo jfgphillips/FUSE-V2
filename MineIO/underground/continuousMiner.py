3a7c1e662d81284445c5fcaf8d170a74fbed0119
jfgphillips <jfgphillips@gmail.com>
2021-07-15 10:39:16 +0100

    implemented room and pillar method with all the qMachines

--- MineIO/underground/continuousMiner.py
+++ MineIO/underground/continuousMiner.py
@@ -1,16 +1,21 @@
 import numpy as np
+import pandas as pd
+from calculators.QMachines import poweredVehicle
 
-class continuousMiner(object):
+
+class continuousMiner_att(object):
     def __init__(self):
-        self.df = pd.read_excel(r'data/mine_attributes.xlsx', sheet_name="continuous miner", skiprows=1)
+        self.df = pd.read_excel(r'/Users/john/Documents/University Work/summer internship/FUSE/data/mine_attributes.xlsx', sheet_name="continuous miner", skiprows=1)
         self.df.set_index('key', inplace=True)
         # print(self.df.index)
         self.production_output = self.df['value'].loc["production output"]
-        self.usage = self.df['value'].loc["usage"]
-        self.maintenance = self.df['value'].loc["maintenance"]
         self.power = self.df['value'].loc["power"]
         self.workers = self.df['value'].loc['workers']
         return
 
-def qMachine(machine_properties):
-    return np.nan
+
+def qMachine(power, required_output, prod_output, usage, units):
+    miner_factor = required_output/prod_output
+    emissions = poweredVehicle(power, usage, units) * miner_factor
+
+    return emissions
