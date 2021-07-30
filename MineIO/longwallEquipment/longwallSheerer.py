ff30f13ffb01979d0e9aaa113037400623f855fa
jfgphillips <jfgphillips@gmail.com>
2021-07-30 11:32:52 +0100

    implemented the IO for longwall method

--- MineIO/longwallEquipment/longwallSheerer.py
+++ MineIO/longwallEquipment/longwallSheerer.py
@@ -1,6 +1,17 @@
 import pandas as pd
+from calculators.QMachines import poweredVehicle
 
 
 class longwallShearer_att:
     def __init__(self):
         df= pd.read_excel(r'/Users/john/Documents/University Work/summer internship/FUSE/data/mine_attributes.xlsx', sheet_name="longwall shearer", skiprows=1)
+        df.set_index('key', inplace=True)
+        self.production_output = df['value'].loc["production output"]
+        self.power = df['value'].loc["power"]
+        self.workers = df['value'].loc["workers"]
+        return
+
+def qMachine(power, required_output, prod_output, usage, units):
+    miner_factor = required_output/prod_output
+    emissions = poweredVehicle(power, usage, units) * miner_factor
+    return emissions
