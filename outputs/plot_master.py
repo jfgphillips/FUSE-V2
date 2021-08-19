eb9ef583c40ad4d7b2253c0d15b2660fe3144cde
jfgphillips <jfgphillips@gmail.com>
2021-08-19 17:19:15 +0100

    made a plot function converted costs into a data frame for more consistent file handling

--- outputs/plot_master.py
+++ outputs/plot_master.py
@@ -1,26 +1,19 @@
 from Geothermal_extraction.LDH import Plot as LHD_plt
 from undergroundMining import Plots as Na_mining_plt
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+def plot(df, title):
+    df_sorted = df.sort_values('$ cost')
+    plt.figure(figsize=(24, 14))
+    my_plot = df_sorted.plot.bar(use_index=True,
+                          stacked=True)
+    my_plot.set_ylabel("cost")
+    plt.title(title)
+    plt.xticks(rotation=0,
+               fontsize=5)
+    plt.legend(loc=0)
+    plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    print("nothing yet")
