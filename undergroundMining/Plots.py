964ea1c6175d86ea74a246b2fff52336e9c12277
jfgphillips <jfgphillips@gmail.com>
2021-08-23 10:50:37 +0100

    made a plot function converted costs into a data frame for more consistent file handling

--- undergroundMining/Plots.py
+++ undergroundMining/Plots.py
@@ -7,6 +7,11 @@ class plot:
     def __init__(self):
         self.all_data = rpm()
         self.emissions_data = self.all_data.total_emissions_df.div(self.all_data.required_tonnes_per_year).mul(365)
+        self.longwall_df = pd.DataFrame(data={"miner_emissions": [149],
+                                                 "support_emissions": [8],
+                                                 'transportation_emissions': [21]},
+                                           index=['longwall method'])
+        self.emissions_data_2 = pd.concat([self.emissions_data, self.longwall_df])
         self.emissions_boxplot()
         return
 
@@ -15,14 +20,15 @@ class plot:
         colours = ['#88CCEE', '#CC6677', '#DDCC77']
         labels = ['miner', 'support', 'transportation']
 
-        myplot = self.emissions_data.plot(use_index=True,
+        myplot = self.emissions_data_2.plot(use_index=True,
                                           kind="bar",
                                           stacked=True,
                                           color=colours, y=fields)
         myplot.set_ylabel("Emissions (kWhr/tonne)")
         plt.title("Mining technique")
         plt.xticks(rotation=0)
-        plt.legend(loc=0)
+        plt.legend().remove()
+        #plt.legend(loc=0)
         plt.show()
         return
 
