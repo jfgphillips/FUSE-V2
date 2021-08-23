84b0ef2e4a26163e52a7b497f589c0268ed915be
BaerCMI <chantalbaer@gmx.net>
2021-08-23 18:22:40 +0100

    correction of units of Li2CO3, implementation of belt conveyor to LDH energy

--- Geothermal_extraction/LDH/Reactant_flow.py
+++ Geothermal_extraction/LDH/Reactant_flow.py
@@ -14,8 +14,8 @@ class ReactantFlow(object):
         self.brine = Brine.Brine_att()
         self.plant = Plant.Plant_att()
         self.FO = ForwardOsmosis.ForwardOsmosis()
-        self.LiCl_extracted = self.brine.Li_conc_brine * ((self.plant.brine_flow_day * 10**3)/ 24) *\
-                              self.column.Li_recovery * self.FO.efficiency * self.plant.plant_uptime  # in ton
+        self.LiCl_extracted = ((self.brine.Li_conc_brine * ((self.plant.brine_flow_day * 10**3) / 24) *
+                               self.plant.plant_uptime) * self.column.Li_recovery * self.FO.efficiency)   # in g/year
         LC_processing_kwargs = {'LiCl': self.LiCl_extracted, 'Yield': self.LC_processing.Yield}
         self.LC_processing_reactants, self.LC_processing_product, self.LC_processing_waste = \
             LC_processing.Li2CO3_reaction(**LC_processing_kwargs)
