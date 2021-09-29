2d871130a14f6796dd04e4c5b06f395ca3e243b3
jfgphillips <jfgphillips@gmail.com>
2021-09-29 08:54:39 +0100

    updated plots

--- undergroundMining/Plots.py
+++ undergroundMining/Plots.py
@@ -8,16 +8,41 @@ class plot:
     def __init__(self):
         self.rpm_data = rpm()
         self.lwm_data = lwm()
+
         self.emissions_data = pd.concat([self.rpm_data.total_emissions_df, self.lwm_data.total_emissions_df], axis=0)
+        self.standardised_emissions_data = self.emissions_data.div(self.rpm_data.required_tonnes_per_year).mul(365)
+
         self.opex_data = pd.concat([self.rpm_data.opex_df, self.lwm_data.opex_df], axis=0)
         self.standardised_opex_data = self.opex_data
-        self.standardised_emissions_data = self.emissions_data.div(self.rpm_data.required_tonnes_per_year).mul(365)
+
+        self.capex_data = pd.concat([self.rpm_data.capex_df, self.lwm_data.capex_df], axis=0)
+        self.standardised_capex_data = self.capex_data
+
         self.emissions_boxplot()
         self.opex_boxplot()
+        self.capex_boxplot()
         return
 
+    def capex_boxplot(self):
+        fields = [c for c in self.standardised_capex_data.columns if c not in 'total capex']
+        # colours = ['#88CCEE', '#CC6677']
+        # labels = ['labour', 'utility']
+
+        myplot = self.standardised_capex_data.plot(use_index=True,
+                                                  kind="bar",
+                                                  stacked=True,
+                                                  y=fields)
+        myplot.set_ylabel("capex ($)")
+        plt.title("Mining technique")
+        plt.xticks(rotation=0)
+        # plt.legend().remove()
+        # plt.legend(labels)
+        plt.show()
+        return
+
+
     def emissions_boxplot(self):
-        fields = ['miner_emissions', 'support_emissions', 'transportation_emissions']
+        fields = [c for c in self.standardised_emissions_data.columns if c not in 'sum']
         colours = ['#88CCEE', '#CC6677', '#DDCC77']
         labels = ['miner', 'support', 'transportation']
 
@@ -34,19 +59,19 @@ class plot:
         return
 
     def opex_boxplot(self):
-        fields = ['labour_costs', 'utility_costs']
-        colours = ['#88CCEE', '#CC6677']
-        labels = ['labour', 'utility']
+        fields = [c for c in self.standardised_opex_data.columns if c not in 'total opex']
+        # colours = ['#88CCEE', '#CC6677']
+        # labels = ['labour', 'utility']
 
         myplot = self.standardised_opex_data.plot(use_index=True,
                                                   kind="bar",
                                                   stacked=True,
-                                                  color=colours, y=fields)
-        myplot.set_ylabel("opex (kWhr/tonne)")
+                                                  y=fields)
+        myplot.set_ylabel("opex ($/tonne)")
         plt.title("Mining technique")
         plt.xticks(rotation=0)
         #plt.legend().remove()
-        plt.legend(labels)
+        #plt.legend(labels)
         plt.show()
         return
 
